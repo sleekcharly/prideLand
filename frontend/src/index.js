33eee07b4b540eb526dc2792459829cbcbc3c329
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-17 19:45:26 +0100

    set up base structure for our application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
+// import react and RaectDOM
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// import react-router
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  // wrap entire application with Router
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
