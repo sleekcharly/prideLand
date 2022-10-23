c79ff52c6cecd30291f65bcbf3fbf2ce41dcaceb
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-10-23 14:51:55 +0100

    set up sanity search query for categoryID

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+
+    if (categoryId) {
+    } else {
+    }
+  }, [categoryId]);
+
+  if (loading)
+    return <Spinner message="We are adding new ideas to your feed" />;
+
   return <div>Feed</div>;
 };
 
